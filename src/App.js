204ca4c8f6858fec547e07df3c33a2de8630f571
Henry Nguyen <knguyen@icstars.org>
2024-03-01 00:19:30 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import LandingPage from 'views/LandingPage';
-import AboutPage from 'views/AboutPage';
-import ServicesPage from 'views/ServicesPage';
-import ProductsPage from 'views/ProductsPage';
-import ContactPage from 'views/ContactPage';
-import Footer from 'views/Footer';
+import React from "react";
+import LandingPage from "views/LandingPage";
+import AboutPage from "views/AboutPage";
+import ServicesPage from "views/ServicesPage";
+import ProductsPage from "views/ProductsPage";
+import ContactPage from "views/ContactPage";
+import Footer from "views/Footer";
 import Helmet from "react-helmet";
-import customer from 'customer.json';
-import "assets/scss/material-kit-pro-react.scss?v=1.9.0";
+import customer from "customer.json";
 
 export default function App() {
-  let city = customer.address.split(' ').splice(4, 1).join()
-  let state = customer.address.split(' ').splice(5, 1).join()
-  let zipCode = customer.address.split(' ').splice(6, 1).join()
-
+  let city = customer.address.split(" ").splice(4, 1).join();
+  let state = customer.address.split(" ").splice(5, 1).join();
+  let zipCode = customer.address.split(" ").splice(6, 1).join();
 
   return (
     <>
@@ -24,7 +22,5 @@ export default function App() {
       <ContactPage />
       <Footer />
     </>
-  )
+  );
 }
-
-
